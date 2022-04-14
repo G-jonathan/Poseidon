b66c8f328c5afecf472e069de438df4c6df74173
jonathan <jonathan.developpeur@gmail.com>
2022-04-14 11:10:27 +0200

    implement_a_feature_trade

--- src/main/java/com/openclassroomsprojet/poseidon/controlllers/TradeController.java
+++ src/main/java/com/openclassroomsprojet/poseidon/controlllers/TradeController.java
@@ -1,6 +1,8 @@
 package com.openclassroomsprojet.poseidon.controlllers;
 
 import com.openclassroomsprojet.poseidon.domain.Trade;
+import com.openclassroomsprojet.poseidon.service.ITradeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,45 +11,56 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class TradeController {
-    // TODO: Inject Trade service
+    @Autowired
+    private ITradeService tradeService;
 
     @RequestMapping("/trade/list")
-    public String home(Model model)
-    {
-        // TODO: find all Trade, add to model
+    public String home(Model model) {
+        model.addAttribute("trade", tradeService.findAllTrade());
         return "trade/list";
     }
 
     @GetMapping("/trade/add")
-    public String addUser(Trade bid) {
+    public String addUser(Trade trade) {
         return "trade/add";
     }
 
     @PostMapping("/trade/validate")
-    public String validate(@Valid Trade trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Trade list
+    public String validate(@Valid Trade trade, BindingResult bindingResult, Model model) {
+        if (!bindingResult.hasErrors()) {
+            tradeService.saveTrade(trade);
+            return "redirect:/trade/list";
+        }
         return "trade/add";
     }
 
     @GetMapping("/trade/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Trade by Id and to model then show to the form
+        Optional<Trade> trade = tradeService.findTradeById(id);
+        model.addAttribute("curvePoint", trade.orElseThrow(() -> new IllegalArgumentException("Invalid tarde Id:" + id)));
         return "trade/update";
     }
 
     @PostMapping("/trade/update/{id}")
-    public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
-                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Trade and return Trade list
+    public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade, BindingResult result, Model model) {
+        if (!result.hasErrors()) {
+            tradeService.saveTrade(trade);
+        }
         return "redirect:/trade/list";
     }
 
     @GetMapping("/trade/delete/{id}")
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Trade by Id and delete the Trade, return to Trade list
+        Optional<Trade> trade = tradeService.findTradeById(id);
+        if (trade.isPresent()) {
+            tradeService.deleteTradeById(id);
+        } else {
+            throw new IllegalArgumentException("Trade id not found");
+        }
         return "redirect:/trade/list";
     }
 }
